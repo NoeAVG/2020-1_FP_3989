5e995582a7442bcaadade5ea8ee44ba233c7cf63
NoeAVG <54225361+NoeAVG@users.noreply.github.com>
2019-10-17 21:38:50 -0500

    Update and rename TAREAS/3/main.c to  TAREAS/ 3/main.c
    Corrección de i a f

--- TAREAS/ 3/main.c
+++ TAREAS/ 3/main.c
@@ -3,17 +3,17 @@ int main(int argc, char *argv[])
 {
 	int n1, n2, suma = 0, resta = 0, multiplicacion = 0, division = 0;
 
-	scanf("%i", &n1);
-	scanf("%i", &n2);
+	scanf("%f", &n1);
+	scanf("%f", &n2);
 
 	suma = n1 + n2;
 	resta = n1 - n2;
 	multiplicacion = n1 * n2;
 	division = n1 / n2;
-	printf("suma: %.2i\n", suma);
-	printf("resta: %.2i\n", resta);
-	printf("multiplicacion: %.2i\n", multiplicacion);
-	printf("division: %.2i\n", division);
+	printf("suma: %.2f\n", suma);
+	printf("resta: %.2f\n", resta);
+	printf("multiplicacion: %.2f\n", multiplicacion);
+	printf("division: %.2f\n", division);
 
 	return 0;
 }
