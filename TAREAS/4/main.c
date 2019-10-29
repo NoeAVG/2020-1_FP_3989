4029cf0001258ae068c006837de90df68312db16
NoeAVG <54225361+NoeAVG@users.noreply.github.com>
2019-10-29 17:05:17 -0600

    Add files via upload

---
+++ TAREAS/4/main.c
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+        //Declaramos y asignamos varibles
+	int lista[argc], suma = 0;
+	float promedio;
+    //Programa
+    for (int i = 1; i < argc; i++){
+    lista[i]=atoi(argv[i]);
+    }
+    for (int i = 0; i < argc; i++){
+    suma = suma + lista[i];	
+    }
+	promedio = (float)suma/(argc - 1);
+	
+        //Mostramos valores
+        printf("%f\n", promedio);
+	return 0;
+}
