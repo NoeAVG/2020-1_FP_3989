48c8e3c025a66224181afe27539b8e6677d1807f
NoeAVG <54225361+NoeAVG@users.noreply.github.com>
2019-11-07 19:38:28 -0600

    Update main.c

--- TAREAS/8/main.c
+++ TAREAS/8/main.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 int main(int argc, char const *argu[])
 {
+	//Asignamos variables
 	int x;
 	float c, f, resultado=0;
-
+        //asignamos valores
 	c = atof(argu[1]);
 	x=c;
 	f = atof(argu[2]);
+	//Funcion
 	switch(x){
 		case 1:
 		    resultado=f*1.8;
 		    resultado= resultado+32;
+			//Imprimimos el resultado de conversion de centigrados a fahrenheit
 		    printf("%.2f\n", resultado);
 		    break;
 		case 2:
 		    resultado=f-32;
 		    resultado=resultado/1.8;
+			//imprimimos el resultado de conversion de fahrenheit a centigrados
 		    printf("%.2f\n", resultado);
 		    break; 
 		}
